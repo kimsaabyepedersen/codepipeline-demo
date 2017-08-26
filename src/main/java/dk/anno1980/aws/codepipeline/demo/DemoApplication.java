9ca7f1eace77839e03ac58348fc4dee692280516
Kim Saabye Pedersen <k.epost@gmail.com>
2017-08-26 13:31:04 +0200

    First commit

---
+++ src/main/java/dk/anno1980/aws/codepipeline/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package dk.anno1980.aws.codepipeline.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	@RestController
+	@RequestMapping(path = "/")
+	class MyController{
+
+		@GetMapping
+		String hello(){
+			return "Hello world";
+		}
+
+	}
+}
